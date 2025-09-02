422b48db0fe97a5acc8b07abe7677399ef906470
Szabó Áron <szabo.aaron@gmail.com>
2025-09-02 12:34:09 +0200

    Add files via upload

---
+++ P00_SzerverKeszites_reszletes(VIZSGAMINTA).js
@@ -0,0 +1,18 @@
+var http = require("http");// beépített HTML modul importálása
+
+const hostname = "127.0.0.1"; //A szerver IP címének beállítása(LOCALHOST)
+
+const port = 3000; //A szerver potja amin elérhető lesz (számként jobb megadni, de ha string az se baj)
+
+
+//Szerver létrehozása arrow functionnal
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;//Státuszkód megadása szerver felé, 200 = minden rendben
+    res.setHeader("Content-Type", "text/plain");//Tartalom típusának beállítása
+    res.end("Szeretem a NodeJS");//Üzenet átadása az oldalra, és kapcsolat lezárása
+});
+
+//A szerver felülegyel a megadott porton, hostnéven indításkor visszajelez a konzol felületre, a szerver címével
+server.listen(port, hostname, () => {
+    console.log(`A szerverünk fut a http://${hostname}:${port} címen`);
+})
